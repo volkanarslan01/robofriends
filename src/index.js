b03240caddd65dc02459515a64b0732585ef4744
Volkan Arslan <101024530+volkanarslan01@users.noreply.github.com>
2022-09-26 12:44:17 +0300

    Robofriends Designer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+ import Card from './card.js';
 import reportWebVitals from './reportWebVitals';
-
+import 'tachyons'; 
+import {robots} from './robots.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+    <Card id = {robots[0].id}name = {robots[0].name} email = {robots[0].email}/>
+    <Card id = {robots[1].id}name = {robots[1].name} email = {robots[1].email}/>
+    <Card id = {robots[2].id}name = {robots[2].name} email = {robots[2].email}/>
+    </div>
   </React.StrictMode>
 );
 
