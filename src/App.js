573d8dc3b11546d0f1539a66bb5cc03ebe8455ee
Volkan Arslan <101024530+volkanarslan01@users.noreply.github.com>
2022-10-09 14:15:22 +0300

    APİ use fetch robots

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import CardList from './CardList.js';
 
 class App extends React.Component {
     
+  
     constructor(){
     super()
     this.state = {
-    robots: robots,
+    robots: [],
     searchField: ''
-    }
+    }}
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users=> this.setState({robots: users}));
     }
     onsearchChange = (event) => {
     this.setState({searchField: event.target.value})
@@ -21,13 +26,19 @@ class App extends React.Component {
            const robotsFilter = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
             });
-        return (
-            <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox searchChange= {this.onsearchChange}/>
-            <CardList robots = {robotsFilter}/>
-            </div>
-         );
+           if(this.state.robots.length ===0){
+            return <h1>Loading...</h1>
+           }
+           else{
+            return (
+                <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange= {this.onsearchChange}/>
+                <CardList robots = {robotsFilter}/>
+                </div>
+             );
+           }
+      
     }
 
 }
