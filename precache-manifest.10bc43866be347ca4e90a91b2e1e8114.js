adb851f514f2bf926446af550cb8d1815a91ef7a
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2022-10-15 00:23:40 +0300

    Updates

---
+++ precache-manifest.10bc43866be347ca4e90a91b2e1e8114.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6c8aaba24dc8037b22c",
+    "url": "/robofriends/static/css/main.37f70ef5.chunk.css"
+  },
+  {
+    "revision": "f6c8aaba24dc8037b22c",
+    "url": "/robofriends/static/js/main.f6c8aaba.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/robofriends/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "e1f66cff1c407aa0f0cc",
+    "url": "/robofriends/static/css/2.c2cbfa88.chunk.css"
+  },
+  {
+    "revision": "e1f66cff1c407aa0f0cc",
+    "url": "/robofriends/static/js/2.e1f66cff.chunk.js"
+  },
+  {
+    "revision": "db08b936970c55357179",
+    "url": "/robofriends/static/js/runtime~main.db08b936.js"
+  },
+  {
+    "revision": "02109eb4ee2b3e7837d7d7b081b327cb",
+    "url": "/robofriends/index.html"
+  }
+];
